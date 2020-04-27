f5e765900e62a41788ad7ebace34a993515e5064
Lenny Capellan <3219530+lencap@users.noreply.github.com>
2020-04-27 08:04:54 -0400

    Update linux.sh

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # linux.sh
+# Run me as:
+# bash <(curl -fsSL https://raw.githubusercontent.com/lencap/osimages/master/scripts/linux.sh)
 
 echo "Setup Linux bash, and other minor shell settings."
 
